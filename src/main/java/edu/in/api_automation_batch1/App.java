fa3fc159ce88c52eeb04f42d898203949e6a221c
S <490723@cognizant.com>
2025-08-05 12:00:44 +0530

    first commit

---
+++ src/main/java/edu/in/api_automation_batch1/App.java
@@ -0,0 +1,13 @@
+package edu.in.api_automation_batch1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
